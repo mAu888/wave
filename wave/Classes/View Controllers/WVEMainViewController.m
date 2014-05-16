1bcecee5cc7a4aa8ffa835eebfa74d4872526b2b
Mauricio Hanika <m.hanika@mytaxi.com>
2014-05-16 13:35:30 +0200

    Some more stuff done <3

--- wave/Classes/View Controllers/WVEMainViewController.m
+++ wave/Classes/View Controllers/WVEMainViewController.m
@@ -17,15 +17,21 @@ NSString *const WVEBeaconIdentifierString = @"io.waveapp.ios.wave";
 ////////////////////////////////////////////////////////////////////////////////
 @interface WVEMainViewController () <CBPeripheralManagerDelegate, CLLocationManagerDelegate>
 
+@property(weak, nonatomic) IBOutlet UIButton *startWavingButton;
+@property(weak, nonatomic) IBOutlet UIButton *stopWavingButton;
 @property(weak, nonatomic) IBOutlet UITextView *logTextView;
 @property(strong, nonatomic) CBPeripheralManager *peripheralManager;
 @property(strong, nonatomic) CLLocationManager *locationManager;
+@property(strong, nonatomic) NSSet *knownBeacons;
 
 @end
 
 
 ////////////////////////////////////////////////////////////////////////////////
 @implementation WVEMainViewController
+{
+    CLBeaconRegion *_beaconRegion;
+}
 
 - (id)init
 {
@@ -33,7 +39,7 @@ NSString *const WVEBeaconIdentifierString = @"io.waveapp.ios.wave";
 
     if ( self )
     {
-
+        _knownBeacons = [NSSet set];
     }
 
     return self;
@@ -42,10 +48,37 @@ NSString *const WVEBeaconIdentifierString = @"io.waveapp.ios.wave";
 
 #pragma mark - Wave
 
-- (IBAction)startTheWave:(id)sender
+- (IBAction)startWaving:(UIButton *)button
 {
     [self startBeaconAdvertising];
     [self startListeningForBeacons];
+
+    self.startWavingButton.hidden = YES;
+    self.stopWavingButton.hidden = NO;
+}
+
+- (IBAction)stopWaving:(UIButton *)button
+{
+    [self stopBeaconAdvertising];
+    [self stopListeningForBeacons];
+
+    self.startWavingButton.hidden = NO;
+    self.stopWavingButton.hidden = YES;
+}
+
+- (void)startBeaconAdvertising
+{
+    NSMutableDictionary *peripheralData = [[self beaconRegion] peripheralDataWithMeasuredPower:nil];
+    self.peripheralManager = [[CBPeripheralManager alloc]
+        initWithDelegate:self queue:dispatch_get_main_queue()];
+
+    [self.peripheralManager startAdvertising:peripheralData];
+}
+
+- (void)stopBeaconAdvertising
+{
+    [self.peripheralManager stopAdvertising];
+    self.peripheralManager = nil;
 }
 
 - (void)startListeningForBeacons
@@ -53,36 +86,27 @@ NSString *const WVEBeaconIdentifierString = @"io.waveapp.ios.wave";
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
 
-    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:WVEBeaconUUIDString];
-    CLBeaconRegion *region = [[CLBeaconRegion alloc]
-        initWithProximityUUID:uuid identifier:WVEBeaconIdentifierString];
-    [self.locationManager startMonitoringForRegion:region];
-    [self.locationManager startRangingBeaconsInRegion:region];
+    [self.locationManager startMonitoringForRegion:[self beaconRegion]];
+    [self.locationManager startRangingBeaconsInRegion:[self beaconRegion]];
 }
 
-- (void)startBeaconAdvertising
+- (void)stopListeningForBeacons
 {
-    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:WVEBeaconUUIDString];
-    CLBeaconRegion *region = [[CLBeaconRegion alloc]
-        initWithProximityUUID:uuid identifier:WVEBeaconIdentifierString];
-
-    NSMutableDictionary *peripheralData = [region peripheralDataWithMeasuredPower:nil];
-    self.peripheralManager = [[CBPeripheralManager alloc]
-        initWithDelegate:self queue:dispatch_get_main_queue()];
-
-    [self.peripheralManager startAdvertising:peripheralData];
+    [self.locationManager stopMonitoringForRegion:[self beaconRegion]];
+    [self.locationManager stopRangingBeaconsInRegion:[self beaconRegion]];
+    self.locationManager = nil;
 }
-
 
 #pragma mark - CBPeripheralManagerDelegate
+
 
 - (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral
 {
 
 }
 
-
 #pragma mark - CLLocationManagerDelegate
+
 
 - (void)locationManager:(CLLocationManager *)manager
         didRangeBeacons:(NSArray *)beacons
@@ -90,12 +114,41 @@ NSString *const WVEBeaconIdentifierString = @"io.waveapp.ios.wave";
 {
     __block NSString *logString = @"";
     [beacons enumerateObjectsUsingBlock:^(CLBeacon *beacon, NSUInteger idx, BOOL *stop) {
+        __block BOOL isKnownBeacon = NO;
+        [self.knownBeacons enumerateObjectsUsingBlock:^(CLBeacon *knownBeacon, BOOL *innerStop) {
+            if ( [knownBeacon.major isEqualToNumber:beacon.major]
+                && [knownBeacon.minor isEqualToNumber:beacon.minor]
+                && [knownBeacon.proximityUUID isEqual:beacon.proximityUUID] )
+            {
+                isKnownBeacon = YES;
+                *innerStop = YES;
+            }
+        }];
+
+        if ( !isKnownBeacon )
+        {
+            self.knownBeacons = [self.knownBeacons setByAddingObject:beacon];
+            self.logTextView.text = [NSString stringWithFormat:@"New beacon: %d.%d\n%@", [beacon.major intValue], [beacon.minor intValue], self.logTextView.text];
+
+            [self triggerLocalNotification];
+        }
+
+
+        ////////////////////////////////////////
         switch ( beacon.proximity )
         {
-            case CLProximityUnknown: logString = [@"unknown\n" stringByAppendingString:logString]; break;
-            case CLProximityImmediate: logString = [@"immediate\n" stringByAppendingString:logString]; break;
-            case CLProximityNear: logString = [@"near\n" stringByAppendingString:logString]; break;
-            case CLProximityFar: logString = [@"far\n" stringByAppendingString:logString]; break;
+            case CLProximityUnknown:
+                logString = [@"unknown\n" stringByAppendingString:logString];
+                break;
+            case CLProximityImmediate:
+                logString = [@"immediate\n" stringByAppendingString:logString];
+                break;
+            case CLProximityNear:
+                logString = [@"near\n" stringByAppendingString:logString];
+                break;
+            case CLProximityFar:
+                logString = [@"far\n" stringByAppendingString:logString];
+                break;
         }
     }];
 
@@ -107,6 +160,34 @@ rangingBeaconsDidFailForRegion:(CLBeaconRegion *)region
                      withError:(NSError *)error
 {
     self.logTextView.text = [NSString stringWithFormat:@"Error: %@\n%@", error, self.logTextView.text];
+}
+
+
+#pragma mark - Private methods
+
+- (CLBeaconRegion *)beaconRegion
+{
+    if ( !_beaconRegion )
+    {
+        CLBeaconMajorValue majorBeaconValue = ( CLBeaconMajorValue ) (arc4random() % UINT16_MAX);
+        CLBeaconMinorValue minorBeaconValue = ( CLBeaconMinorValue ) (arc4random() % UINT16_MAX);
+
+        NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:WVEBeaconUUIDString];
+        _beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
+                                                                major:majorBeaconValue
+                                                                minor:minorBeaconValue
+                                                           identifier:WVEBeaconIdentifierString];
+    }
+
+    return _beaconRegion;
+}
+
+- (void)triggerLocalNotification
+{
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    notification.alertBody = NSLocalizedString(@"There's somebody near you", nil);
+    notification.fireDate = [NSDate date];
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
 }
 
 @end
---
+++ waveTests/WVEMainViewControllerTests.m
@@ -0,0 +1,35 @@
+//
+//  WVEMainViewControllerTests.m
+//  wave
+//
+//  Created by Maurício Hanika on 16.05.14.
+//  Copyright (c) 2014 wave. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@import CoreLocation;
+
+@interface WVEMainViewControllerTests : XCTestCase
+
+@end
+
+@implementation WVEMainViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+}
+
+@end
